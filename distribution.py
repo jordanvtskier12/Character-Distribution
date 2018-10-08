5a5eda309946dcc6df4a63c061159a4be862c02f
jordanvtskier12 <42844401+jordanvtskier12@users.noreply.github.com>
2018-10-08 10:15:31 -0400

    Update distribution.py

--- distribution.py
+++ distribution.py
@@ -36,3 +36,42 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+"""
+vowels = ['e', 'a', 'u', 'o', 'i']
+vowels.sort
+ <bound method list.<native JS> of ['e', 'a', 'u', 'o', 'i']>
+print('Sorted list:', vowels)
+('Sorted list:', ['e', 'a', 'u', 'o', 'i'])
+vowels.sort(reverse=True)
+print vowels
+grej=['4a','3b','2k','4c']
+vowels.sort()
+letlist=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+letlist.append(
+ParseError: bad input on line 2
+1 : evaluationresult = letlist.append(
+2>: if not evaluationresult == None: print repr(evaluationresult)
+letlist.append('f')
+instring=('hello aaa doff')
+cnta=instring.count("a")
+print cnta
+cnta=instring.count("b")
+print cnta
+bignumlist=[0]*26
+twoway=zip(letlist,bignumlist)
+print twoway
+for i in range(1:bignumlist)
+ParseError: bad input on line 1
+1>: evaluationresult = for i in range(1:bignumlist)
+2 : if not evaluationresult == None: print repr(evaluationresult)
+for i in range(len(bignumlist)):
+	temp=instring.cnt(twoway[0,i])
+	twoway[1,i]=temp
+ 	
+   
+twoway[1,i]=temp
+help for
+twoway[1,i]=temp
+	
+	print(i)
+ for i in range(0,25):
